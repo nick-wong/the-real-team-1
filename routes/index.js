504addd2847cf5a92a2452118af9c36ce56b5cb8
Nicholas Wong <nic147919@gmail.com>
2018-02-15 03:56:16 -0800

    Added more JSON/handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
+var data = require("../data.json");
 
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
+  var items = data.items;
+  var recommendations = data.recommendations;
+
+  var recommendations_list = [];
+  items.forEach(function(obj) {
+    if (recommendations.includes(obj.id)) {
+      recommendations_list.push(obj);
+    }
+  });
   var name = "test";
 
-  console.log(name);
-  res.render('index', {'name': name, 'layout': 'defaultLayout'});
+  console.log(recommendations_list);
+  res.render('index', {'name': name, 'layout': 'defaultLayout', 'recommendations': recommendations_list});
 };
