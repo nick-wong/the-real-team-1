3c9c5cc12a7f5a7151c867ea94a87b7cd777d3cd
Nicholas Wong <nic147919@gmail.com>
2018-02-20 15:17:43 -0800

    Top deals sorted page

--- routes/top-deals.js
+++ routes/top-deals.js
@@ -1,3 +1,27 @@
+var data = require('../data.json');
+
 exports.view = function(req, res){
-  res.render('top-deals', {'layout' : 'defaultLayout'});
+  var items = data.items;
+  var itemSavings = [];
+  var itemSavingsSorted = [];
+  var topItems = [];
+
+  items.forEach(function(obj) {
+    var percentageOff = Math.round(((obj.old_price - obj.new_price)/obj.old_price)*100);
+
+    //console.log("og price: " + obj.old_price + ", new: " + obj.new_price + ", % off: " + percentageOff);
+    itemSavings.push(percentageOff);
+  });
+
+  itemSavingsSorted = itemSavings.slice();
+  itemSavingsSorted.sort(function(a, b){return b - a});
+
+  //itemSavings
+  console.log(itemSavings);
+  for(i = 0; i < 5; i++) {
+    topItems.push(items[itemSavings.indexOf(itemSavingsSorted[i])]);
+  }
+  //console.log(topItems);
+
+  res.render('top-deals', {'layout' : 'defaultLayout', 'topItems': topItems});
 };
