9b4d32ee5c83a401da614be467fa6f26be4b7a2c
JarninFang <jarnin.fang@servicenow.com>
2018-02-22 23:36:38 -0800

    cahnge

--- config/auth.js
+++ config/auth.js
@@ -7,7 +7,7 @@ module.exports = {
     'clientID'      : '140437863440312', // your App ID
     'clientSecret'  : '25056ca7d9079d4215209ac2b26164f5', // your App Secret
     //'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
-    'callbackURL'   : 'http://the-real-team-1.herokuapp.com/auth/facebook/callback',
+    'callbackURL'   : 'http://a7-the-real-team-1.herokuapp.com/auth/facebook/callback',
     'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
   },
 };
